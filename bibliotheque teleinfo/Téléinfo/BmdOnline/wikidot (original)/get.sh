1a2036c4bf6aae28fbed83e011d5121f2b834f17
BmdOnline <vpc@bourmad.com>
2017-02-21 19:01:01 +0100

    Ressources
    Branche dédiée pour les ressources, afin de réduire le volume du projet
    lors des téléchargements.

--- bibliotheque teleinfo/Téléinfo/BmdOnline/wikidot (original)/get.sh
+++
@@ -1,3 +0,0 @@
-wget http://hallard.wikidot.com/local--files/teleinfo-broadcast/teleinfo -O - | iconv --from-code UTF-16 --to-code ASCII --output teleinfo.sh
-wget 'http://hallard.wikidot.com/local--files/teleinfo-broadcast/Makefile' -O - | iconv --from-code UTF-16 --to-code ASCII --output Makefile
-wget http://hallard.wikidot.com/local--files/teleinfo-broadcast/teleinfo.c
