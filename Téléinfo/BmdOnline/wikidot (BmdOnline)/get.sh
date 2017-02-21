7cb85f0d5bace824dc1f7312c2de949772b2dcc0
BmdOnline <vpc@bourmad.com>
2017-02-21 18:40:25 +0100

    Initial Ressources

---
+++ Téléinfo/BmdOnline/wikidot (BmdOnline)/get.sh
@@ -0,0 +1,3 @@
+wget http://hallard.wikidot.com/local--files/teleinfo-broadcast/teleinfo -O -|iconv -f UTF-16 -t UTF8 --output teleinfo.sh
+wget 'http://hallard.wikidot.com/local--files/teleinfo-broadcast/Makefile' -O -|iconv -f UTF-16 -t UTF8 --output Makefile
+wget http://hallard.wikidot.com/local--files/teleinfo-broadcast/teleinfo.c -O -|iconv -f UTF-16 -t UTF8 --output teleinfo.c
